890a1363ca7900744cd527438cb2186990db2f17
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-04-29 19:24:02 +0700

    Perbaikan soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <math.h>
+#include <iostream> // perbaikan dari #Include <Stdio.H>
+#include <math.h> // perbaikan dari #inlcude "math.h"
 using namespace std;
 
-int main(void) {
+int main(void) { //perbaikan dari float main(void)
     int n;
     int prima = 1;
 
-    system("cls");
+    system("cls"); //perbaikan dari SYSTEM("CLS"):
 
     cout << "Insert a number: ";
-    cin >> n;
+    cin >> n; //perbaikan dari cin << n;
 
-    if (n <= 1) {
+    if (n <= 1) { //perbaikan dari if (n >= 1)
+        prima = 0;
         cout << n << " bukanlah bilangan prima.";
     } else {
-        for (int i = 2; i < n; ++i) {
-            if (n % i == 0) {
-                  prima = 0;
-                  break;
+        for (int i = 2; i <= sqrt(n); i++) { //perbaikan dari i = 1 
+            if (n % i == 0) { //perbaikan dari (n % 1 == 2)
+                prima = 0;
+                cout << n << " bukanlah bilangan prima."; // perbaikan dari cout >> n << " bukanlah bilangan prima.";
+                break;
             }
         }
     }
 
         if (prima) {
-            cout << n << " merupakan bilangan prima.";
-        } else {
-            cout << n << " bukanlah bilangan prima.";
+            cout << n << " merupakan bilangan prima."; //perbaikan dari coute >> n << "Merupakan bilangan prima.";
         }
             
     return 1;
